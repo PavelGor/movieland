1d3bf551b81c73f4c5ccff285c880262de983283
Pavel Gordeev <gordeevp@gmail.com>
2018-11-02 15:02:49 +0200

    Add user and requestId logging

--- movieland-web/src/main/java/com/gordeev/movieland/configuration/AppServletConfig.java
+++ movieland-web/src/main/java/com/gordeev/movieland/configuration/AppServletConfig.java
@@ -1,11 +1,18 @@
 package com.gordeev.movieland.configuration;
 
+import com.gordeev.movieland.interceptor.LoggingInterceptor;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"com.gordeev.movieland.controller"})
-public class AppServletConfig {
+public class AppServletConfig implements WebMvcConfigurer{
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggingInterceptor()).addPathPatterns("/**");
+    }
 }
---
+++ movieland-web/src/main/java/com/gordeev/movieland/interceptor/LoggingInterceptor.java
@@ -0,0 +1,32 @@
+package com.gordeev.movieland.interceptor;
+
+import com.gordeev.movieland.entity.User;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.UUID;
+
+@Component
+public class LoggingInterceptor  extends HandlerInterceptorAdapter {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        if (user != null){
+            MDC.put("user", user.getEmail());
+        } else {
+            MDC.put("user", "Guest");
+        }
+        MDC.put("requestId", UUID.randomUUID().toString());
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)  {
+        MDC.clear();
+    }
+}
